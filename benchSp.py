4aa2169d007fe80f024faea3b680b3e63a856e31
Yunfan Zhang <yunfanz@berkeley.edu>
2017-06-26 03:13:19 -0700

    benching with batchsize

--- benchSp.py
+++ benchSp.py
@@ -20,7 +20,7 @@ def reorder_matrix(matrix, symm=True):
     return new_matrix
 
 if False:
-	shape = (60000,6500) 
+	shape = (60000,1500) 
 	A = np.random.random(shape).astype(np.float32)
 	b = np.random.random(shape[0]).astype(np.float32)
 	T_transfer, T_compute = [], []
@@ -55,47 +55,51 @@ if False:
 
 # Radio astronomy bench
 nfreqs = np.arange(1,5,2)
-nants = 350
-nvis = 6000
+nants = 100
+nvis = 2000
 ncols = nants+nvis
-batch = 128
+
 
 T_transfer, T_compute, Asize = [], [], []
 
-print "Preparing data"
-nrows = nants*(nants-1)/2
-Asize.append(nrows*nants)
-rows = np.repeat(np.arange(nrows), 3).astype(np.int32)
-cols = np.zeros_like(rows)
-data = np.ones_like(rows).astype(np.float32)
-b = np.random.random(nrows).astype(np.float32)
+batches = np.array([16, 32, 64, 128, 256, 512])
+for batch in batches:
+	print "Preparing data"
+	nrows = nants*(nants-1)/2
+	Asize.append(nrows*nants)
+	rows = np.repeat(np.arange(nrows), 3).astype(np.int32)
+	cols = np.zeros_like(rows)
+	data = np.ones_like(rows).astype(np.float32)
+	b = np.random.random(nrows).astype(np.float32)
 
-i = 0
-for pair in itertools.combinations(np.arange(nants), 2):
-	cols[i], cols[i+1] = pair[0], pair[1]
-	cols[i+2] = np.random.choice(nvis) + nants
-	i += 3
+	i = 0
+	for pair in itertools.combinations(np.arange(nants), 2):
+		cols[i], cols[i+1] = pair[0], pair[1]
+		cols[i+2] = np.random.choice(nvis) + nants
+		i += 3
 
-A = csr_matrix( (data,(rows,cols)), shape=(nrows,ncols) ).todense()
-AtA = np.dot(A.T, A)
-Atb = np.asarray(np.dot(A.T, b)).flatten()
-AtAcsr = reorder_matrix(coo_matrix(AtA))
-dataA = np.hstack([AtAcsr.data for i in xrange(batch)])
-datab = np.hstack([Atb for i in xrange(batch)])
+	A = csr_matrix( (data,(rows,cols)), shape=(nrows,ncols) ).todense()
+	AtA = np.dot(A.T, A)
+	Atb = np.asarray(np.dot(A.T, b)).flatten()
+	AtAcsr = reorder_matrix(coo_matrix(AtA))
 
-print "got AtA with sparsity", 1 - float(AtAcsr.nnz)/(AtAcsr.shape[0]**2)
-#import IPython; IPython.embed()
-solver = gpusolver.SpSolver(AtA.shape[0], AtA.shape[0], batch)
-solver.prepare_workspace(AtAcsr.indptr, AtAcsr.indices)
-t0 = time.time()
-solver.from_csr(dataA, datab)
-t1 = time.time()
-x = solver.solve_Axb_and_retrieve()
-t2 = time.time()
-T_compute.append((t2-t1))
-T_transfer.append((t1-t0))
-print "compute per sample: ", T_compute[0]/batch
-print "transfer per sample: ", T_transfer[0]/batch
+	#import IPython; IPython.embed()
+	dataA = np.hstack([AtAcsr.data for i in xrange(batch)])
+	datab = np.hstack([Atb for i in xrange(batch)])
+
+	print "got AtA with sparsity", 1 - float(AtAcsr.nnz)/(AtAcsr.shape[0]**2)
+	#import IPython; IPython.embed()
+	solver = gpusolver.SpSolver(AtA.shape[0], AtA.shape[0], batch)
+	solver.prepare_workspace(AtAcsr.indptr, AtAcsr.indices)
+	t0 = time.time()
+	solver.from_csr(dataA, datab)
+	t1 = time.time()
+	x = solver.solve_Axb_and_retrieve()
+	t2 = time.time()
+	T_compute.append((t2-t1))
+	T_transfer.append((t1-t0))
+print "compute per sample: ", np.array(T_compute)/batches
+print "transfer per sample: ", T_transfer
 # plt.figure()
 # plt.plot(Asize, T_compute, label='compute')
 # plt.plot(Asize, T_transfer, label='tranfer')
