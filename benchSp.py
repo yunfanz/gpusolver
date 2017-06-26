4fbcfdbac5fd51bcdf55b5030f811cc9ad82130e
Yunfan Zhang <yunfanz@berkeley.edu>
2017-06-26 13:38:16 -0700

    reordering benchmarks ready

--- benchSp.py
+++ benchSp.py
@@ -17,7 +17,14 @@ def reorder_matrix(matrix, symm=True):
         (matrix.data, (perm_i, perm_j)), 
         shape=matrix.shape
     )
-    return new_matrix
+    return new_matrix, rcm_perm
+
+def reorder_vector(vec, rcm_perm, reverse=False):
+ 	if reverse:
+ 		rev_perm_dict = {k : rcm_perm.tolist().index(k) for k in rcm_perm}
+ 		return np.array([vec[rev_perm_dict[k]] for k in xrange(vec.size)])
+ 	else:
+ 		return np.array([vec[rcm_perm[k]] for k in xrange(vec.size)])
 
 if False:
 	shape = (60000,1500) 
@@ -56,33 +63,39 @@ if False:
 # Radio astronomy bench
 nfreqs = np.arange(1,5,2)
 nants = 100
-nvis = 2000
+nvis = 500
 ncols = nants+nvis
 
 
 T_transfer, T_compute, Asize = [], [], []
 
+
+print "Preparing data"
+nrows = nants*(nants-1)/2
+Asize.append(nrows*nants)
+rows = np.repeat(np.arange(nrows), 3).astype(np.int32)
+cols = np.zeros_like(rows)
+data = np.ones_like(rows).astype(np.float32)
+b = np.random.random(nrows).astype(np.float32)
+
+i = 0
+for pair in itertools.combinations(np.arange(nants), 2):
+	cols[i], cols[i+1] = pair[0], pair[1]
+	cols[i+2] = np.random.choice(nvis) + nants
+	i += 3
+
+A = csr_matrix( (data,(rows,cols)), shape=(nrows,ncols) ).todense()
+AtA = np.dot(A.T, A)
+Atb_ = np.asarray(np.dot(A.T, b)).flatten()
+AtAcsr, rcm_perm = reorder_matrix(coo_matrix(AtA))
+Atb = reorder_vector(Atb_, rcm_perm, False)
+#Atb = Atb_
+#import IPython; IPython.embed()
+#AtAcsr = csr_matrix(AtA)
+x_np = np.asarray(np.dot(np.linalg.pinv(AtA), Atb_)).flatten()
+
 batches = np.array([16, 32, 64, 128, 256, 512])
 for batch in batches:
-	print "Preparing data"
-	nrows = nants*(nants-1)/2
-	Asize.append(nrows*nants)
-	rows = np.repeat(np.arange(nrows), 3).astype(np.int32)
-	cols = np.zeros_like(rows)
-	data = np.ones_like(rows).astype(np.float32)
-	b = np.random.random(nrows).astype(np.float32)
-
-	i = 0
-	for pair in itertools.combinations(np.arange(nants), 2):
-		cols[i], cols[i+1] = pair[0], pair[1]
-		cols[i+2] = np.random.choice(nvis) + nants
-		i += 3
-
-	A = csr_matrix( (data,(rows,cols)), shape=(nrows,ncols) ).todense()
-	AtA = np.dot(A.T, A)
-	Atb = np.asarray(np.dot(A.T, b)).flatten()
-	AtAcsr = reorder_matrix(coo_matrix(AtA))
-
 	#import IPython; IPython.embed()
 	dataA = np.hstack([AtAcsr.data for i in xrange(batch)])
 	datab = np.hstack([Atb for i in xrange(batch)])
@@ -94,10 +107,15 @@ for batch in batches:
 	t0 = time.time()
 	solver.from_csr(dataA, datab)
 	t1 = time.time()
-	x = solver.solve_Axb_and_retrieve()
+	#x = solver.solve_Axb_and_retrieve()
+	x = solver.solve_Axb_and_retrieve()[:AtA.shape[1]]
 	t2 = time.time()
 	T_compute.append((t2-t1))
 	T_transfer.append((t1-t0))
+	print x.shape, x_np.shape
+	x = reorder_vector(x, rcm_perm, True)
+	#import IPython; IPython.embed()
+	print 'Relative Error', np.sum((np.asarray(np.dot(AtA, x))[0]-Atb_)**2)/np.sum(Atb**2)
 print "compute per sample: ", np.array(T_compute)/batches
 print "transfer per sample: ", T_transfer
 # plt.figure()
